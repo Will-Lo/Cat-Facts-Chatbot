57776fbcb20de095b226c8e2f00537787d0a6bb0
William Lo <will-i-am_95@hotmail.com>
2016-09-24 00:04:15 -0400

    uploaded bot

---
+++ index.js
@@ -0,0 +1,216 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+const catFacts = require('cat-facts');
+const token = "EAAWcSPE8EKsBAE6Al4gydYltgID5u1fnEZCKZAxtWc5sOTYAau44zPCGx7lV3tTlzz8L1a8CR15RcupRpZCtUfwTwTkwoOKPYAZBSYW9YYqZB915acDmIluFZBgeT9SN4Du4YWJibdixb712vaQO5AubBzjNlcyF0ZBSSg0MluPTwZDZD";
+const Wit = require('node-wit').Wit;
+const wit_token = 'RJ3JZM5PJP56CEXGNIQPEWB2LSIWAJDL';
+const verify_token = 'chatbot232';
+const fb_page_id = '1537899319849708'
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 3000));
+
+app.get('/', function (req, res) {
+    res.send('This is the cat\'s den');
+});
+
+const fbReq = request.defaults({
+  uri: 'https://graph.facebook.com/me/messages',
+  method: 'POST',
+  json: true,
+  qs: { access_token: token },
+  headers: {'Content-Type': 'application/json'},
+});
+
+// Facebook Webhook
+app.get('/webhook', (req, res) => {
+  if (!FB_VERIFY_TOKEN) {
+    throw new Error('missing FB_VERIFY_TOKEN');
+  }
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === verify_token) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+const fbMessage = (recipientId, msg, cb) => {
+  const opts = {
+    form: {
+      recipient: {
+        id: recipientId,
+      },
+      message: {
+        text: msg,
+      },
+    },
+  };
+  fbReq(opts, (err, resp, data) => {
+    if (cb) {
+      cb(err || data.error && data.error.message, data);
+    }
+  });
+};
+
+const getFirstMessagingEntry = (body) => {
+  const val = body.object == 'page' &&
+    body.entry &&
+    Array.isArray(body.entry) &&
+    body.entry.length > 0 &&
+    body.entry[0] &&
+    body.entry[0].id === fb_page_id &&
+    body.entry[0].messaging &&
+    Array.isArray(body.entry[0].messaging) &&
+    body.entry[0].messaging.length > 0 &&
+    body.entry[0].messaging[0]
+  ;
+  return val || null;
+};
+
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {fbid: facebookUserId, context: sessionState}
+const sessions = {};
+
+const findOrCreateSession = (fbid) => {
+  let sessionId;
+  //check if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // got session
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // no session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
+  }
+  return sessionId;
+};
+
+
+
+
+const actions = {
+  say(sessionId, context, message, cb) {
+    // retrieve the Facebook user
+    const recipientId = sessions[sessionId].fbid;
+    if (recipientId) {
+      // found recipient
+      fbMessage(recipientId, message, (err, data) => {
+        if (err) {
+          console.log(
+            'Oops! An error occurred while forwarding the response to',
+            recipientId,
+            ':',
+            err
+          );
+        }
+        cb();
+      });
+    } else {
+      console.log('Oops! Couldn\'t find user for session:', sessionId);
+      cb();
+    }
+  },
+  merge(sessionId, context, entities, message, cb) {
+    cb(context);
+  },
+  error(sessionId, context, error) {
+    console.log(error.message);
+  },
+  'sendsCatFact':(sessionId, context, cb) => {
+    context.message = catFacts.random();
+    cb(context);
+  }
+};
+
+const wit = new Wit(wit_token, actions);
+
+app.post('/webhook', (req, res) => {
+  // parsing Messenger API response
+  const messaging = getFirstMessagingEntry(req.body);
+  if (messaging && messaging.message && messaging.recipient.id === fb_page_id) {
+    // got message and retrieve the Facebook user ID of the sender
+    const sender = messaging.sender.id;
+    // retrieve user sesssion
+    const sessionId = findOrCreateSession(sender);
+
+    // We retrieve the message content
+    const msg = messaging.message.text;
+    const atts = messaging.message.attachments;
+
+    if (atts) {
+      // handle attachments
+      fbMessage(
+        sender,
+        'Sorry! I can only process text messages for now.'
+      );
+    } else if (msg) {
+      // received a text message
+      // forward the message to the Wit.ai Bot Engine
+      wit.runActions(
+        sessionId, // the user's current session
+        msg, // the user's message 
+        sessions[sessionId].context, // the user's current session state
+        (error, context) => {
+          if (error) {
+            console.log('Oops! Got an error from Wit:', error);
+          } else {
+            // waiting for further messages to proceed.
+            console.log('Waiting for futher messages.');
+
+            // updating the user's current session state
+            sessions[sessionId].context = context;
+          }
+        }
+      );
+    }
+  }
+  res.sendStatus(200);
+});
+
+function kittenMessage(recipientId, text) {
+    //adds picture of kitten but doesn't work at the moment
+    text = text || "";
+    var values = text.split(' ');
+    
+    if (values.length === 3 && values[0] === 'kitten') {
+        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
+            
+            var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
+            
+            message = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Kitten",
+                            "subtitle": "Cute kitten picture",
+                            "image_url": imageUrl ,
+                            "buttons": [{
+                                "type": "web_url",
+                                "url": imageUrl,
+                                "title": "Show kitten"
+                                }, {
+                                "type": "postback",
+                                "title": "I like this",
+                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                            }]
+                        }]
+                    }
+                }
+            };
+            sendMessage(recipientId, message);
+            return true;
+        }
+    }
+    
+    return false;
+};
